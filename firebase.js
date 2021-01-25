82456f7b4b88e16ea37c25239c5446a6a0297461
Victor Ribeiro <vvictorrib@gmail.com>
2021-01-24 21:48:26 -0300

    enviroment file added

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCRfxgiYyIgrK8IMy8otwLXoLJH29MKbSI",
-    authDomain: "petmagnet-421d0.firebaseapp.com",
-    databaseURL: "https://petmagnet-421d0.firebaseio.com",
-    projectId: "petmagnet-421d0",
-    storageBucket: "petmagnet-421d0.appspot.com",
-    messagingSenderId: "759305058777",
-    appId: "1:759305058777:web:67a3c6afc5b91fbcc881c8",
-    measurementId: "G-L09TBGR221"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig)
